153743e9521cb499c4df092c49ac5272341ecd60
Fabian Carlström <inrick@gmail.com>
2018-11-20 16:59:38 +0100

    ch. 6-7

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> // drand48 seems to require gnu extension
 
 // vector begin
 typedef struct {
@@ -79,13 +80,37 @@ typedef struct {
   float radius;
 } sphere;
 
+typedef struct {
+  v3 llc; // lower left corner
+  v3 horiz;
+  v3 vert;
+  v3 origin;
+} camera;
+
+ray camera_ray_at_xy(camera *c, float x, float y) {
+  // llc + x*horiz + y*vert - origin
+  v3 dir = v3_sub(
+      v3_add(c->llc, v3_add(v3_kmul(x, c->horiz), v3_kmul(y, c->vert))),
+      c->origin);
+  return (ray){.A = c->origin, .B = dir};
+}
+
 void raytrace1(void);
 void raytrace2(void);
+void raytrace3(void);
 void ppm_gradient(void);
 
 int main() {
-  raytrace2();
+  raytrace3();
   return 0;
+}
+
+v3 random_in_unit_ball() {
+  v3 u;
+  do {
+    u = v3_sub(v3_kmul(2, (v3){drand48(), drand48(), drand48()}), v3_one);
+  } while (v3_norm(u) >= 1.0);
+  return u;
 }
 
 // The out parameter hit_record will be written to if function returns true
@@ -131,6 +156,58 @@ bool hit_sphere_arr(sphere s[], size_t nspheres, ray *r, float tmin, float tmax,
     }
   }
   return hit_obj;
+}
+
+v3 ray_color3(ray *r, sphere s[], size_t nspheres) {
+  hit_record rec;
+  // apparently one clips slightly above 0 to avoid "shadow acne"
+  if (hit_sphere_arr(s, nspheres, r, 0.001, FLT_MAX, &rec)) {
+    v3 target = v3_add(v3_add(rec.p, rec.normal), random_in_unit_ball());
+    ray r2 = {.A = rec.p, .B = v3_sub(target, rec.p)};
+    return v3_kmul(0.5, ray_color3(&r2, s, nspheres));
+  }
+  float t = 0.5*(v3_normalize(r->B).y + 1.0);
+  return v3_add(
+      v3_kmul(1.0-t, v3_one),
+      v3_kmul(t, (v3){0.5, 0.7, 1.0}));
+}
+
+void raytrace3(void) {
+  size_t nx = 600;
+  size_t ny = 300;
+  size_t ns = 100;
+
+  camera cam = {
+    .llc    = {-2.0, -1.0, -1.0},
+    .horiz  = {4.0, 0.0, 0.0},
+    .vert   = {0.0, 2.0, 0.0},
+    .origin = v3_zero,
+  };
+  sphere spheres[] = {
+    {.center = {0,0,-1},      .radius = 0.5},
+    {.center = {0,-100.5,-1}, .radius = 100},
+  };
+  size_t nspheres = 2;
+
+  printf("P3\n%zu %zu\n255\n", nx, ny);
+  for (size_t j = ny; j > 0; j--) {
+    for (size_t i = 0; i < nx; i++) {
+      v3 color = v3_zero;
+      // anti-alias by averaging color around random nearby samples
+      for (size_t s = 0; s < ns; s++) {
+        float x = (float)(i+drand48()) / (float)nx;
+        float y = (float)(j-1+drand48()) / (float)ny;
+        ray r = camera_ray_at_xy(&cam, x, y);
+        color = v3_add(color, ray_color3(&r, spheres, nspheres));
+      }
+      color = v3_kdiv(color, (float)ns);
+      color = (v3){sqrt(color.x), sqrt(color.y), sqrt(color.z)};
+      int ir = (int)(255.99 * color.x);
+      int ig = (int)(255.99 * color.y);
+      int ib = (int)(255.99 * color.z);
+      printf("%d %d %d\n", ir, ig, ib);
+    }
+  }
 }
 
 v3 ray_color2(ray *r, sphere s[], size_t nspheres) {
