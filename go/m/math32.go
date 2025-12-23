52e0083f0cccea41a498699e9967325198f789ce
Fabian Carlström <inrick@gmail.com>
2025-12-23 13:12:20 +0100

    Remove comment
    It is not an appropriate solution when using gc compiler either. Casting
    the expression naively is already rewritten by the compiler into one
    operation/instruction where appropriate:
    
    https://github.com/golang/go/blob/2cd0371a0a61e0dec6034239b1a92a20968c8522/src/cmd/compile/internal/ssa/_gen/generic.rules#L1303
    https://github.com/golang/go/blob/2cd0371a0a61e0dec6034239b1a92a20968c8522/src/cmd/compile/internal/ssa/_gen/AMD64.rules#L120
    https://github.com/golang/go/blob/2cd0371a0a61e0dec6034239b1a92a20968c8522/src/cmd/compile/internal/ssa/_gen/ARM64.rules#L62

--- go/m/math32.go
+++ go/m/math32.go
@@ -20,17 +20,3 @@ func init() {
 func Abs(x Float) Float {
 	return math.Float32frombits(math.Float32bits(x) &^ (1 << 31))
 }
-
-// tinygo does not support the go assembler so just use a simple wrapper for
-// sqrt as well.
-//func Sqrt32(x float32) float32
-/*
-#include "textflag.h"
-
-// func Sqrt32(x float32) float32
-TEXT ·Sqrt32(SB), NOSPLIT, $0
-	XORPS  X0, X0 // break dependency
-	SQRTSS x+0(FP), X0
-	MOVSS  X0, ret+8(FP)
-	RET
-*/
